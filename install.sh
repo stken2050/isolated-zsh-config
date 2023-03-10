e855b4535f8fb1122c6bf47afc395202751a48dc
stken2050 <stken2050@gmail.com>
2023-03-10 09:22:27 +0900

    Initial commit

---
+++ install.sh
@@ -0,0 +1,23 @@
+# rm and ls should not use directory/, just directly
+
+echo '=== INSTALLING ZSH ==='
+
+sudo rm -rf \
+$HOME/.zshrc \
+$HOME/.zsh_history \
+$HOME/.p10k.zsh \
+$HOME/.zsh \
+$HOME/.fonts
+
+ln -s $(pwd)/homeuser/.zshrc $HOME/.zshrc
+ln -s $(pwd)/homeuser/.zsh_history $HOME/.zsh_history
+ln -s $(pwd)/homeuser/.p10k.zsh $HOME/.p10k.zsh
+ln -s $(pwd)/homeuser/.zsh $HOME/.zsh
+ln -s $(pwd)/homeuser/.fonts $HOME/.fonts
+
+sudo fc-cache
+
+sh -c $(pwd)/homeuser/.zsh/powerlevel10k/gitstatus/install
+
+echo 'done.' 
+
